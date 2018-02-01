42a56ee3c3ea2fc45bc0b72432154ef5c1b4eecb
Will Olson <will.olson11@gmail.com>
2018-02-01 11:04:26 -0800

    feat: Add brew bundle command to install script

--- install.sh
+++ install.sh
@@ -6,25 +6,28 @@ git pull origin master;
 git submodule init && git submodule update
 
 function perform() {
-  rsync --exclude ".git/" \
-    --exclude ".gitmodules" \
-    --exclude ".DS_Store" \
-    --exclude ".osx" \
-    --exclude "install.sh" \
-    --exclude "README.md" \
-    --exclude "LICENSE.md" \
-    -avh --no-perms . ~;
-  source ~/.bash_profile;
+    brew bundle install
+
+    rsync --exclude ".git/" \
+        --exclude ".gitmodules" \
+        --exclude ".DS_Store" \
+        --exclude ".osx" \
+        --exclude "install.sh" \
+        --exclude "README.md" \
+        --exclude "LICENSE.md" \
+        -avh --no-perms . ~;
+
+    source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  perform;
-else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-  echo "";
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
     perform;
-  fi;
+else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        perform;
+    fi;
 fi;
 
 unset perform;
